453ea84c4f6b8b34e959066759f9b8c82b104800
Tianyi Ge <tianyig@outlook.com>
2021-04-08 13:17:19 +0800

    concurrent gpu inference

--- serverless/deploy_gpu.sh
+++ serverless/deploy_gpu.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Sample commands to deploy nuclio functions on GPU
 
+fn_name=$2
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 nuctl create project cvat
 
 if [ "$1" = "tf-faster-rcnn-inception-v2-coco" ]; then
-  nuctl deploy --project-name cvat \
+  nuctl deploy $fn_name --project-name cvat \
     --path "$SCRIPT_DIR/tensorflow/faster_rcnn_inception_v2_coco/nuclio" \
     --platform local --base-image tensorflow/tensorflow:2.1.1-gpu \
     --desc "GPU based Faster RCNN from Tensorflow Object Detection API" \
@@ -15,7 +17,7 @@ if [ "$1" = "tf-faster-rcnn-inception-v2-coco" ]; then
     --resource-limit nvidia.com/gpu=1 --verbose
 
 elif [ "$1" = 'tf-matterport-mask-rcnn' ]; then
-  nuctl deploy --project-name cvat \
+  nuctl deploy $fn_name --project-name cvat \
     --path "$SCRIPT_DIR/tensorflow/matterport/mask_rcnn/nuclio" \
     --platform local --base-image tensorflow/tensorflow:1.15.5-gpu-py3 \
     --desc "GPU based implementation of Mask RCNN on Python 3, Keras, and TensorFlow." \
--- serverless/run.sh
+++ serverless/run.sh
@@ -3,10 +3,16 @@ fn_names=("tf-faster-rcnn-inception-v2-coco" "tf-matterport-mask-rcnn")
 
 repeat_num=$1
 
+function run() {
+  fn_name=$1
+  i=$2
+  { time sudo ./deploy_gpu.sh $fn_name $fn_name$i; } 2>&1 >/dev/null | grep real | awk '{print $2}' | sudo tee -a $fn_name.deploy.txt
+  { time cat /tmp/input.json | sudo nuctl invoke $fn_name$i -c 'application/json'; } 2>&1 >/dev/null | grep real | awk '{print $2}' | sudo tee -a $fn_name.invoke.txt
+  sudo nuctl delete functions $fn_name$i >/dev/null
+}
+
 for ((i = 1; i <= repeat_num; ++i)); do
   for fn_name in ${fn_names[@]}; do
-    { time sudo ./deploy_gpu.sh $fn_name; } 2>&1 >/dev/null | grep real | awk '{print $2}' | sudo tee -a $fn_name.deploy.txt
-    { time cat /tmp/input.json | sudo nuctl invoke $fn_name -c 'application/json'; } 2>&1 >/dev/null | grep real | awk '{print $2}' | sudo tee -a $fn_name.invoke.txt
-    sudo nuctl delete functions $fn_name >/dev/null
+    run $fn_name $i &
   done
 done
